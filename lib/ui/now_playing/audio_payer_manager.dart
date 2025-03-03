18018106e1dfa81995c3d78830eb53756c3490b8
HoangTuan2112 <tuanmaiyeunguoido@gmail.com>
2025-03-03 15:19:23 +0700

    thiet ke co ban now playing

---
+++ lib/ui/now_playing/audio_payer_manager.dart
@@ -0,0 +1,35 @@
+import 'package:just_audio/just_audio.dart';
+import 'package:rxdart/rxdart.dart';
+
+class AudioPlayerManager{
+  AudioPlayerManager({
+    required this.songUrl,
+  });
+  final player = AudioPlayer();
+  Stream<DurationState>? durationState;
+  String songUrl;
+  void init(){
+    durationState = Rx.combineLatest2<Duration,PlaybackEvent,DurationState>(
+      player.positionStream,player.playbackEventStream,
+        (positon,playbackEvent) => DurationState(
+          progress: positon,
+          buffered: playbackEvent.bufferedPosition,
+          total: playbackEvent.duration,
+        ));
+    player.setUrl(songUrl);
+
+  }
+}
+class DurationState {
+  final dynamic buffered;
+
+  final dynamic progress;
+
+  final dynamic total;
+
+  const DurationState({
+    required this.progress,
+    required this.buffered,
+    this.total,
+  });
+}
--- lib/ui/now_playing/playing.dart
+++ lib/ui/now_playing/playing.dart
@@ -1,6 +1,8 @@
+import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:music_app/data/model/song.dart';
-import 'package:music_app/ui/home/viewmodel.dart';
+import 'package:music_app/ui/now_playing/audio_payer_manager.dart';
 
 class NowPlaying extends StatelessWidget {
   const NowPlaying({super.key, required this.playingSong, required this.songs});
@@ -10,12 +12,14 @@ class NowPlaying extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  NowPlayingPage(playingSong: playingSong, songs: songs);
+    return NowPlayingPage(playingSong: playingSong, songs: songs);
   }
 }
 
 class NowPlayingPage extends StatefulWidget {
-  const NowPlayingPage({super.key, required this.playingSong, required this.songs});
+  const NowPlayingPage(
+      {super.key, required this.playingSong, required this.songs});
+
   final Song playingSong;
   final List<Song> songs;
 
@@ -23,13 +27,215 @@ class NowPlayingPage extends StatefulWidget {
   State<NowPlayingPage> createState() => _NowPlayingPageState();
 }
 
-class _NowPlayingPageState extends State<NowPlayingPage> {
+class _NowPlayingPageState extends State<NowPlayingPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _imageAnimationController;
+  late AudioPlayerManager _audioPlayerManager;
+
+  @override
+  void initState() {
+    super.initState();
+    _imageAnimationController = AnimationController(
+      duration: const Duration(seconds: 12000),
+      vsync: this,
+    );
+    _audioPlayerManager =
+        AudioPlayerManager(songUrl: widget.playingSong.source);
+
+    _audioPlayerManager.init();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Now playing'),
-      ),
+    final screenWidth = MediaQuery.of(context).size.width;
+    const delta = 64;
+    final radius = (screenWidth - delta) / 2;
+    return CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(
+          middle: const Text('Now playing'),
+          trailing: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.more_horiz),
+          ),
+        ),
+        child: Scaffold(
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  widget.playingSong.album,
+                ),
+                const SizedBox(height: 16),
+                Text('_ ___ _'),
+                const SizedBox(height: 48),
+                RotationTransition(
+                  turns: Tween(begin: 0.0, end: 1.0)
+                      .animate(_imageAnimationController),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(radius),
+                    child: FadeInImage.assetNetwork(
+                      placeholder: 'assets/imageIcon.jpg',
+                      image: widget.playingSong.image,
+                      width: screenWidth - delta,
+                      height: screenWidth - delta,
+                      imageErrorBuilder: (context, error, stackStrace) {
+                        return Image.asset(
+                          'assets/imageIcon.jpg',
+                          width: screenWidth - delta,
+                          height: screenWidth - delta,
+                        );
+                      },
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 64, bottom: 16),
+                  child: SizedBox(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.share_outlined),
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                        Column(
+                          children: [
+                            Text(
+                              widget.playingSong.title,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyMedium!
+                                  .copyWith(
+                                      color: Theme.of(context)
+                                          .textTheme
+                                          .bodyMedium!
+                                          .color),
+                            ),
+                            const SizedBox(height: 8),
+                            Text(
+                              widget.playingSong.artist,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyMedium!
+                                  .copyWith(
+                                      color: Theme.of(context)
+                                          .textTheme
+                                          .bodyMedium!
+                                          .color),
+                            ),
+                          ],
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.favorite_outline),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      top: 32, right: 24, left: 24, bottom: 16),
+                  child: _progressBar(),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      top: 32, right: 24, left: 24, bottom: 16),
+                  child: _mediaButtons(),
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+
+  StreamBuilder<DurationState> _progressBar() {
+    return StreamBuilder<DurationState>(
+      stream: _audioPlayerManager.durationState,
+      builder: (context, snapshot) {
+        final durationState = snapshot.data;
+        final progress = durationState?.progress ?? Duration.zero;
+        final buffered = durationState?.buffered ?? Duration.zero;
+        final total = durationState?.total ?? Duration.zero;
+        return ProgressBar(
+          progress: progress,
+          buffered: buffered,
+          total: total,
+        );
+      },
+    );
+  }
+}
+Widget _mediaButtons() {
+  return SizedBox(
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        MediaButtonControl(
+          function: () {},
+          icon: Icons.shuffle,
+          color: Colors.deepPurple,
+          size: 24,
+        ),
+        MediaButtonControl(
+          function: () {},
+          icon: Icons.skip_previous,
+          color: Colors.deepPurple,
+          size: 36,
+        ),
+        MediaButtonControl(
+          function: () {},
+          icon: Icons.play_arrow_sharp,
+          color: Colors.deepPurple,
+          size: 48,
+        ),
+        MediaButtonControl(
+          function: () {},
+          icon: Icons.skip_next,
+          color: Colors.deepPurple,
+          size: 36,
+        ),
+        MediaButtonControl(
+          function: () {},
+          icon: Icons.repeat,
+          color: Colors.deepPurple,
+          size: 24,
+        ),
+      ],
+    ),
+  );
+}
+
+class MediaButtonControl extends StatefulWidget {
+  const MediaButtonControl({
+    super.key,
+    required this.function,
+    required this.icon,
+    required this.color,
+    required this.size,
+  });
+
+  final void Function()? function;
+  final IconData icon;
+  final Color color;
+  final double size;
+
+  @override
+  State<StatefulWidget> createState() {
+    return _MediaButtonControlState();
+  }
+}
+
+class _MediaButtonControlState extends State<MediaButtonControl> {
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: widget.function,
+      icon: Icon(widget.icon),
+      iconSize: widget.size,
+      color: widget.color??Theme.of(context).colorScheme.primary,
     );
   }
 }
